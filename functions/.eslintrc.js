0eac208033b8f13072846fc6f5c2a4e648524fcb
Phanindra Pvs <vpamidimukka@umass.edu>
2022-12-16 04:49:02 -0500

    added jest(test framework) configuration

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -19,6 +19,8 @@ module.exports = {
   },
   ignorePatterns: [
     '/lib/**/*', // Ignore built files.
+    'jest**',
+    '/test/**',
   ],
   plugins: ['@typescript-eslint', 'import'],
   rules: {
